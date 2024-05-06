8d2108a84b6e52048208c8867c2d5397a62dfbee
Anthony Gaultier <antho_621@yahoo.fr>
2024-05-06 10:34:13 +0200

    updated sql by comment unused

--- sql/dacode_script.sql
+++ sql/dacode_script.sql
@@ -1,49 +1,51 @@
-drop database pedacode;
-
 -- Création base de données
-create database pedacode;
+-- create database pedacode;
 
 -- Utilisation base de donnée
-use dacode;
+use pedacode;
 
 
 -- Suppression tables
-drop table if exists WkPlayg;
-drop table if exists WkLessFork;
-drop table if exists WkLessRepo;
-drop table if exists Langage;
-drop table if exists Code;
-drop table if exists Workspace;
-drop table if exists UserProfile;
-drop table if exists Goal;
-drop table if exists Lesson;
-drop table if exists Subscription;
-drop table if exists Category;
-drop table if exists Chapter;
+-- drop table if exists WkPlayg;
+-- drop table if exists WkLessFork;
+-- drop table if exists WkLessModel;
+-- drop table if exists Langage;
+-- drop table if exists Code;
+-- drop table if exists Workspace;
+-- drop table if exists UserProfile;
+-- drop table if exists Goal;
+-- drop table if exists Lesson;
+-- drop table if exists Subscription;
+-- drop table if exists Category;
+-- drop table if exists Chapter;
+
+-- Suppression de trigger / fonctions
+-- drop trigger if exists del_wk_cascade_les;
+-- drop function if exists create_playg_workspace;
+-- drop function if exists create_fork_less_workspace;
 
 
 -- Création tables sur la base
--- Le moteur InnoDB est important pour le que ON DELETE CASCADE fonctionne
-CREATE TABLE Category (
-    id_cat INT AUTO_INCREMENT,
-    name_cat VARCHAR(20) NOT NULL,
-    PRIMARY KEY (id_cat)
-) ENGINE=InnoDB;
+-- CREATE TABLE Category (
+--     id_cat INT AUTO_INCREMENT,
+--     name_cat VARCHAR(20) NOT NULL,
+--     PRIMARY KEY (id_cat)
+-- ) ENGINE=InnoDB;
 
-CREATE TABLE Chapter (
-    id_ch INT AUTO_INCREMENT,
-    id_cat INT,
-    title_ch VARCHAR(20) NOT NULL,
-    PRIMARY KEY (id_ch),
-    FOREIGN KEY (id_cat) REFERENCES Category(id_cat) ON UPDATE CASCADE ON DELETE CASCADE
-) ENGINE=InnoDB;
+-- CREATE TABLE Chapter (
+--     id_ch INT AUTO_INCREMENT,
+--     id_cat INT,
+--     title_ch VARCHAR(20) NOT NULL,
+--     PRIMARY KEY (id_ch),
+--     FOREIGN KEY (id_cat) REFERENCES Category(id_cat) ON UPDATE CASCADE ON DELETE CASCADE
+-- ) ENGINE=InnoDB;
 
-CREATE TABLE Subscription (
-    id_sub INT AUTO_INCREMENT,
-    name_sub VARCHAR(15) NOT NULL,
-    type_sub VARCHAR(12) NOT NULL,
-    PRIMARY KEY (id_sub)
-) ENGINE=InnoDB;
+-- CREATE TABLE Subscription (
+--     id_sub INT AUTO_INCREMENT,
+--     name_sub VARCHAR(15) NOT NULL,
+--     type_sub VARCHAR(12) NOT NULL,
+--     PRIMARY KEY (id_sub)
+-- ) ENGINE=InnoDB;
 
 CREATE TABLE UserProfile (
     id_user INT AUTO_INCREMENT,
@@ -83,26 +85,26 @@ CREATE TABLE Code (
     FOREIGN KEY (id_lang) REFERENCES Langage(id_lang)
 ) ENGINE=InnoDB;
 
-CREATE TABLE Lesson (
-    id_les INT AUTO_INCREMENT,
-    title_les VARCHAR(20),
-    instr_les VARCHAR(4200),
-    modif_les TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    id_ch INT,
-    id_sub INT,
-    PRIMARY KEY (id_les),
-    FOREIGN KEY (id_ch) REFERENCES Chapter(id_ch) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (id_sub) REFERENCES Subscription(id_sub)
-) ENGINE=InnoDB;
+-- CREATE TABLE Lesson (
+--     id_les INT AUTO_INCREMENT,
+--     title_les VARCHAR(20),
+--     instr_les VARCHAR(4200),
+--     modif_les TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+--     id_ch INT,
+--     id_sub INT,
+--     PRIMARY KEY (id_les),
+--     FOREIGN KEY (id_ch) REFERENCES Chapter(id_ch) ON UPDATE CASCADE ON DELETE CASCADE,
+--     FOREIGN KEY (id_sub) REFERENCES Subscription(id_sub)
+-- ) ENGINE=InnoDB;
 
-CREATE TABLE Goal (
-    id_goal INT AUTO_INCREMENT,
-    descr_goal VARCHAR(150),
-    condi_goal VARCHAR(2500),
-    id_les INT,
-    PRIMARY KEY (id_goal),
-    FOREIGN KEY (id_les) REFERENCES Lesson(id_les) ON UPDATE CASCADE ON DELETE CASCADE
-) ENGINE=InnoDB;
+-- CREATE TABLE Goal (
+--     id_goal INT AUTO_INCREMENT,
+--     descr_goal VARCHAR(150),
+--     condi_goal VARCHAR(2500),
+--     id_les INT,
+--     PRIMARY KEY (id_goal),
+--     FOREIGN KEY (id_les) REFERENCES Lesson(id_les) ON UPDATE CASCADE ON DELETE CASCADE
+-- ) ENGINE=InnoDB;
 
 CREATE TABLE WkPlayg (
     id_wkpg INT AUTO_INCREMENT,
@@ -113,31 +115,31 @@ CREATE TABLE WkPlayg (
     FOREIGN KEY (id_wk) REFERENCES Workspace(id_wk) ON UPDATE CASCADE ON DELETE CASCADE
 ) ENGINE=InnoDB;
 
-CREATE TABLE WkLessFork (
-    id_wklesfork INT AUTO_INCREMENT,
-    id_wk INT,
-    id_les INT,
-    is_completed BIT(1) DEFAULT 0,
-    PRIMARY KEY (id_wklesfork),
-    FOREIGN KEY (id_wk) REFERENCES Workspace(id_wk) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (id_les) REFERENCES Lesson(id_les) ON UPDATE CASCADE ON DELETE CASCADE
-) ENGINE=InnoDB;
+-- CREATE TABLE WkLessFork (
+--     id_wklesfork INT AUTO_INCREMENT,
+--     id_wk INT,
+--     id_les INT,
+--     is_completed BIT(1) DEFAULT 0,
+--     PRIMARY KEY (id_wklesfork),
+--     FOREIGN KEY (id_wk) REFERENCES Workspace(id_wk) ON UPDATE CASCADE ON DELETE CASCADE,
+--     FOREIGN KEY (id_les) REFERENCES Lesson(id_les) ON UPDATE CASCADE ON DELETE CASCADE
+-- ) ENGINE=InnoDB;
 
-CREATE TABLE WkLessRepo (
-    id_wklesrepo INT AUTO_INCREMENT,
-    id_wk INT,
-    id_les INT,
-    PRIMARY KEY (id_wklesrepo),
-    FOREIGN KEY (id_wk) REFERENCES Workspace(id_wk) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (id_les) REFERENCES Lesson(id_les) ON UPDATE CASCADE ON DELETE CASCADE
-) ENGINE=InnoDB;
+-- CREATE TABLE WkLessModel (
+--     id_wklesmod INT AUTO_INCREMENT,
+--     id_wk INT,
+--     id_les INT,
+--     PRIMARY KEY (id_wklesmod),
+--     FOREIGN KEY (id_wk) REFERENCES Workspace(id_wk) ON UPDATE CASCADE ON DELETE CASCADE,
+--     FOREIGN KEY (id_les) REFERENCES Lesson(id_les) ON UPDATE CASCADE ON DELETE CASCADE
+-- ) ENGINE=InnoDB;
 
 -- Les indexes
 create index idx_cod ON Code(id_cod);
 create index idx_wk ON Workspace(id_wk);
 create index idx_wk_pg ON WkPlayg(id_wkpg);
-create index idx_wk_les_save ON WkLessFork(id_wklesfork);
-create index idx_wk_les_cr ON WkLessRepo(id_wklesrepo);
+create index idx_wk_les_fork ON WkLessFork(id_wklesfork);
+create index idx_wk_les_modl ON WkLessModel(id_wklesmod);
 create index idx_cat on Category(id_cat, name_cat);
 create index idx_ch on Chapter(id_ch, title_ch);
 create index idx_goal on Goal(id_goal);
@@ -145,12 +147,6 @@ create index idx_sub on Subscription(id_sub);
 create index idx_les on Lesson(id_les);
 create index idx_user on UserProfile(id_user, pseudo_user);
 create index idx_lang on Langage(id_lang);
-
-
--- Suppression de trigger / fonctions
-drop trigger if exists del_wk_cascade_les;
-drop function if exists create_playg_workspace;
-drop function if exists create_fork_less_workspace;
 
 delimiter //
 -- Trigger quand une leçon est supprimée alors les workspaces associés le seront aussi
@@ -174,13 +170,13 @@ BEGIN
 END;
 //
 -- Fonction nécessaire pour créer un workspace repo (leçon créée par un admin), ainsi on récupère l'id
-CREATE FUNCTION create_repo_less_workspace (idUser INT, idLes INT) RETURNS INT
+CREATE FUNCTION create_model_less_workspace (idUser INT, idLes INT) RETURNS INT
 BEGIN
     INSERT INTO Workspace (id_user) VALUES (idUser);
 
     SET @idWk = LAST_INSERT_ID();
 
-    INSERT INTO WkLessRepo (id_wk, id_les) VALUES (@idWk, idLes);
+    INSERT INTO WkLessModel (id_wk, id_les) VALUES (@idWk, idLes);
 
     RETURN @idWk;
 END;
@@ -195,7 +191,7 @@ BEGIN
     INSERT INTO WkLessFork (id_wk, id_les, is_completed) VALUES (@idWk, idLes, is_completed);
 
     -- Cette seconde partie de la fonction sert à récupérer le code pour pour le copier (fork)
-    SELECT id_cod FROM Code WHERE id_wk IN (SELECT id_wk FROM WkLessRepo WHERE WkLessRepo.id_les = idLes)
+    SELECT id_cod FROM Code WHERE id_wk IN (SELECT id_wk FROM WkLessModel WHERE WkLessModel.id_les = idLes)
         INTO @id_cod;
     
     SELECT id_lang, data_cod FROM Code WHERE id_cod = @id_cod
